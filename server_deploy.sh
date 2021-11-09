54d24d6fc8b8e320973178c28479c1f6580096e9
AbulQasem <madrasa.abualqasem@gmail.com>
2021-11-09 22:40:03 +0100

    prueba final

--- server_deploy.sh
+++ server_deploy.sh
@@ -4,26 +4,26 @@ set -e
 echo "Deploying application ..."
  
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+(/usr/bin/php7.4-cli artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
     git fetch origin deploy
     git reset --hard origin/deploy
  
     # Install dependencies based on lock file
-    composer install --no-interaction --prefer-dist --optimize-autoloader
+    /usr/bin/php7.4-cli composer.phar install --no-interaction --prefer-dist --optimize-autoloader
  
     # Migrate database
-    php artisan migrate --force
+    
  
     # Note: If you're using queue workers, this is the place to restart them.
     # ...
  
     # Clear cache
-    php artisan optimize
+    /usr/bin/php7.4-cli artisan optimize
  
     # Reload PHP to update opcache
     echo "" | sudo -S service php7.4-fpm reload
 # Exit maintenance mode
-php artisan up
+/usr/bin/php7.4-cli artisan up
  
 echo "Application deployed!"
